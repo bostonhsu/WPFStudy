b35771547900f133b23763c13ea9236e4abf698a
Boston Hsu <bostonhsu@outlook.com>
2017-07-04 09:45:26 +0800

    通过继承并覆写Application，可以观察到进程中Application对象的生命周期。

--- WPFStudy/WPFStudy/HelloWPF.cs
+++ WPFStudy/WPFStudy/HelloWPF.cs
@@ -11,8 +11,46 @@ namespace WPFStudy
             Window win = new Window();
             win.Title = "WPF application";
             win.Content = "Hello WPF!";
-            Application app = new Application();
+            Application app = new MainApp();
             app.Run(win);
+        }
+
+        public class MainApp : Application
+        {
+            public MainApp() : base()
+            {
+                
+            }
+
+            protected override void OnActivated(EventArgs e)
+            {
+                Console.WriteLine("OnActivated");
+                base.OnActivated(e);
+            }
+
+            protected override void OnExit(ExitEventArgs e)
+            {
+                Console.WriteLine("OnExit");
+                base.OnExit(e);
+            }
+
+            protected override void OnDeactivated(EventArgs e)
+            {
+                Console.WriteLine("OnDeactivated");
+                base.OnDeactivated(e);
+            }
+
+            protected override void OnStartup(StartupEventArgs e)
+            {
+                Console.WriteLine("OnStartup");
+                base.OnStartup(e);
+            }
+
+            protected override void OnSessionEnding(SessionEndingCancelEventArgs e)
+            {
+                Console.WriteLine("OnSessionEnding");
+                base.OnSessionEnding(e);
+            }
         }
     }
 }
