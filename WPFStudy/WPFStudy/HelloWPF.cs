93703db23802a647cfd931188a814226e3353c1e
Boston Hsu <bostonhsu@outlook.com>
2017-07-04 09:32:07 +0800

    第一个例子。

---
+++ WPFStudy/WPFStudy/HelloWPF.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFStudy
+{
+    public class HelloWPF
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Window win = new Window();
+            win.Title = "WPF application";
+            win.Content = "Hello WPF!";
+            win.Show();
+            Application app = new Application();
+            app.Run();
+        }
+    }
+}
